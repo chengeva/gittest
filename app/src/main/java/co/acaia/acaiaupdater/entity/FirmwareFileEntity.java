72b72df33cacb0507bef98d7ef3e545963867ce0
hanjord49 <jordan.han>
2019-07-10 20:07:07 +0800

    Modified realm object

--- app/src/main/java/co/acaia/acaiaupdater/entity/FirmwareFileEntity.java
+++ app/src/main/java/co/acaia/acaiaupdater/entity/FirmwareFileEntity.java
@@ -1,7 +1,15 @@
 package co.acaia.acaiaupdater.entity;
 
-public class FirmwareFileEntity {
+import io.realm.RealmObject;
+
+public class FirmwareFileEntity extends RealmObject {
+    
     public String fileName;
     public String title;
     public String detail;
+    public String shortCap;
+    public int mainVer;
+    public int subVer;
+    public int addVer;
+
 }
