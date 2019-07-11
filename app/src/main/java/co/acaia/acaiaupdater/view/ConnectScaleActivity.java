e59ba9745a2fd5962f38587e2b3d5e66876b489b
hanjord49 <jordan.han>
2019-07-11 22:29:57 +0800

    Added Activities

---
+++ app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
@@ -0,0 +1,15 @@
+package co.acaia.acaiaupdater.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.acaia.acaiaupdater.R;
+
+public class ConnectScaleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connect_to_device);
+    }
+}
---
+++ app/src/main/java/co/acaia/acaiaupdater/view/deviceList/UpdateFirmwareActivity.java
@@ -0,0 +1,15 @@
+package co.acaia.acaiaupdater.view.deviceList;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.acaia.acaiaupdater.R;
+
+public class UpdateFirmwareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.update_fw);
+    }
+}
