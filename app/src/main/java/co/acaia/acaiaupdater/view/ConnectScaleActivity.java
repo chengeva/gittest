ab562c385dd2a2662d0b0826bea438fe0180e12e
hanjord49 <jordan.han>
2019-07-15 11:58:20 +0800

    Prepare to test connection

--- app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
+++ app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
@@ -1,5 +1,9 @@
 package co.acaia.acaiaupdater.view;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +13,15 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import co.acaia.acaiaupdater.R;
+import co.acaia.acaiaupdater.ScaleService;
 import co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice;
 import co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDeviceFactory;
+import co.acaia.communications.events.WeightEvent;
 import co.acaia.communications.scaleService.DistanceConnectEvent;
+import co.acaia.communications.scaleService.ScaleCommunicationService;
+import co.acaia.communications.scaleService.gatt.Log;
+import co.acaia.communications.scaleevent.ScaleSettingUpdateEvent;
+import co.acaia.communications.scaleevent.ScaleSettingUpdateEventType;
 import de.greenrobot.event.EventBus;
 
 public class ConnectScaleActivity extends AppCompatActivity {
@@ -38,6 +48,7 @@ public class ConnectScaleActivity extends AppCompatActivity {
         init_views();
         current_connection_state=STATE_DISCONNECTED;
         update_view_status();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
     }
 
     private void update_view_status(){
@@ -45,6 +56,7 @@ public class ConnectScaleActivity extends AppCompatActivity {
             case STATE_CONNECTED:
                 break;
             case STATE_CONNECTING:
+                tv_Update_status.setText("Connecting to "+currentSelectedDevice.modelName);
                 break;
             case STATE_DISCONNECTED:
                 tv_disconnect.setVisibility(View.GONE);
@@ -62,7 +74,10 @@ public class ConnectScaleActivity extends AppCompatActivity {
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                current_connection_state=STATE_CONNECTING;
+                update_view_status();
                 EventBus.getDefault().post(new DistanceConnectEvent());
+
             }
         });
         tv_Updating_progress=(TextView)findViewById(R.id.Updating_progress);
@@ -70,12 +85,99 @@ public class ConnectScaleActivity extends AppCompatActivity {
         tv_current_firmware=(TextView)findViewById(R.id.tv_current_firmware);
         tv_disconnect=(TextView) findViewById(R.id.tv_disconnect);
     }
-    
+
     @Override
     public boolean onSupportNavigateUp(){
         finish();
         return true;
     }
 
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            // ApplicationUtils.logcat(TAG,"got broadcast event!,action="+String.valueOf(action) );
+            if (ScaleCommunicationService.ACTION_DATA_AVAILABLE.equals(action)) {
+
+                try {
+                    int resultType = intent.getExtras().getInt(
+                            ScaleCommunicationService.EXTRA_DATA_TYPE);
+
+
+                    float val;
+                    // ApplicationUtils.logcat(TAG,"got broadcast event! result type="+String.valueOf(resultType)+" "+"data="+String.valueOf(result) );
+                    switch (resultType) {
+
+                        case ScaleCommunicationService.DATA_TYPE_WEIGHT:
+                            // Log.v(TAG,"got weight!");
+                            String result = intent.getExtras().getString(
+                                    ScaleCommunicationService.EXTRA_DATA);
+                            int unit = intent.getExtras().getInt(ScaleCommunicationService.EXTRA_UNIT);
+                            EventBus.getDefault().post(new ScaleSettingUpdateEvent(ScaleSettingUpdateEventType.event_type.EVENT_UNIT.ordinal(), unit));
+                            if(unit==0){
+                                result=result+"g";
+                            }else{
+                                result=result+"ozWeightEvent";
+                            }
+                            Log.v("ConnectScaleActivity",result);
+
+                            EventBus.getDefault().post(new WeightEvent(result));
+                            break;
+
+                        case ScaleCommunicationService.DATA_TYPE_KEY_DISABLED_ELAPSED_TIME:
+                            val = intent.getExtras().getFloat(
+                                    ScaleCommunicationService.EXTRA_DATA);
+                            EventBus.getDefault().post(new ScaleSettingUpdateEvent(ScaleSettingUpdateEventType.event_type.EVENT_KEY_DISABLED_ELAPSED_TIME.ordinal(), val));
+                            break;
+                        case ScaleCommunicationService.DATA_TYPE_BEEP:
+                            val = intent.getExtras().getFloat(
+                                    ScaleCommunicationService.EXTRA_DATA);
+                            EventBus.getDefault().post(new ScaleSettingUpdateEvent(ScaleSettingUpdateEventType.event_type.EVENT_BEEP.ordinal(), val));
+                            break;
+                        case ScaleCommunicationService.DATA_TYPE_AUTO_OFF_TIME:
+                            val = intent.getExtras().getFloat(
+                                    ScaleCommunicationService.EXTRA_DATA);
+                            EventBus.getDefault().post(new ScaleSettingUpdateEvent(ScaleSettingUpdateEventType.event_type.EVENT_AUTO_OFF_TIME.ordinal(), val));
+                            break;
+                        case ScaleCommunicationService.DATA_TYPE_BATTERY:
+                            val = intent.getExtras().getFloat(
+                                    ScaleCommunicationService.EXTRA_DATA);
+                            EventBus.getDefault().post(new ScaleSettingUpdateEvent(ScaleSettingUpdateEventType.event_type.EVENT_BATTERY.ordinal(), val));
+                            break;
+                    }
+
+                } catch (Exception e) {
+                    // Log.i(TAG, "Error on getting data!");
+                }
+
+
+            }
+        }
+    };
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(ScaleService.ACTION_CONNECTION_STATE_CONNECTED);
+        intentFilter.addAction(ScaleService.ACTION_CONNECTION_STATE_DISCONNECTED);
+        intentFilter.addAction(ScaleService.ACTION_CONNECTION_STATE_DISCONNECTING);
+        intentFilter.addAction(ScaleService.ACTION_CONNECTION_STATE_CONNECTING);
+        intentFilter.addAction(ScaleService.ACTION_SERVICES_DISCOVERED);
+        intentFilter.addAction(ScaleService.ACTION_DATA_AVAILABLE);
+        intentFilter.addAction(ScaleService.ACTION_DEVICE_FOUND);
+        return intentFilter;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        try {
+            unregisterReceiver(mGattUpdateReceiver);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        super.onDestroy();
+    }
 
 }
