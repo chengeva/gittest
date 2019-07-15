4cfdebefcced206d67a5d45c425e712918971e90
hanjord49 <jordan.han>
2019-07-15 11:41:13 +0800

    Updated disconnect statsu

--- app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
+++ app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import co.acaia.acaiaupdater.R;
+import co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice;
+import co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDeviceFactory;
 import co.acaia.communications.scaleService.DistanceConnectEvent;
 import de.greenrobot.event.EventBus;
 
@@ -17,14 +19,42 @@ public class ConnectScaleActivity extends AppCompatActivity {
     private TextView tv_Updating_progress;
     private TextView tv_Update_status;
     private  TextView tv_current_firmware;
+    private  TextView tv_disconnect;
+
+    private AcaiaDevice currentSelectedDevice;
+
+    private static final int STATE_DISCONNECTED=0;
+    private static final int STATE_CONNECTED=1;
+    private static final int STATE_CONNECTING=2;
+
+    private int current_connection_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connect_to_device);
+        currentSelectedDevice= AcaiaDeviceFactory.acaiaDeviceFromModelName(getIntent().getStringExtra("modelName"));
         init_views();
+        current_connection_state=STATE_DISCONNECTED;
+        update_view_status();
     }
 
+    private void update_view_status(){
+        switch (current_connection_state){
+            case STATE_CONNECTED:
+                break;
+            case STATE_CONNECTING:
+                break;
+            case STATE_DISCONNECTED:
+                tv_disconnect.setVisibility(View.GONE);
+                tv_current_firmware.setVisibility(View.GONE);
+                tv_Updating_progress.setText("Connect to "+currentSelectedDevice.modelName);
+                tv_Update_status.setText("Please place your phone close to "+currentSelectedDevice.modelName);
+                break;
+            default:
+                break;
+        }
+    }
     private void init_views()
     {
         connectButton=(Button)findViewById(R.id.btn_connect);
@@ -37,6 +67,6 @@ public class ConnectScaleActivity extends AppCompatActivity {
         tv_Updating_progress=(TextView)findViewById(R.id.Updating_progress);
         tv_Update_status=(TextView)findViewById(R.id.Update_status);
         tv_current_firmware=(TextView)findViewById(R.id.tv_current_firmware);
-        tv_current_firmware.setVisibility(View.GONE);
+        tv_disconnect=(TextView) findViewById(R.id.tv_disconnect);
     }
 }
