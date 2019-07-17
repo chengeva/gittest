ac421dc0746d13ef49a23975becf591eb9505be3
hanjord49 <jordan.han>
2019-07-17 15:53:55 +0800

    Prepare to add instructions

--- app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
+++ app/src/main/java/co/acaia/acaiaupdater/view/ConnectScaleActivity.java
@@ -8,6 +8,8 @@ import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -57,6 +59,8 @@ public class ConnectScaleActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.connect_to_device);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Connect");
+
         currentSelectedDevice= AcaiaDeviceFactory.acaiaDeviceFromModelName(getIntent().getStringExtra("modelName"));
         init_views();
         current_connection_state=STATE_DISCONNECTED;
@@ -66,6 +70,28 @@ public class ConnectScaleActivity extends ActionBarActivity {
         currentFirmwareFileEntity= FirmwareEntityHelper.obtainFirmwareWithModelName(currentSelectedDevice).get(0);
         Log.v("ConnectScaleActivity","Updating to: "+currentFirmwareFileEntity.detail);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_icon2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_info) {
+           
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void onEvent(final DeviceOKEvent deviceOKEvent){
         this.current_connection_state=STATE_CONNECTED;
