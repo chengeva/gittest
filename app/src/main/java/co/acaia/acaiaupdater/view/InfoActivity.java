df1ace126d4ca1d20e996bb1d64d460f54298d3f
hanjord49 <jordan.han>
2019-07-17 15:43:56 +0800

    Added version name

--- app/src/main/java/co/acaia/acaiaupdater/view/InfoActivity.java
+++ app/src/main/java/co/acaia/acaiaupdater/view/InfoActivity.java
@@ -1,19 +1,27 @@
 package co.acaia.acaiaupdater.view;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import co.acaia.acaiaupdater.R;
 
 public class InfoActivity extends ActionBarActivity {
 
+    private TextView tv_version;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Information");
+        tv_version=(TextView)findViewById(R.id.tv_version);
+        tv_version.setText(getVersionName(getApplicationContext()));
     }
 
     @Override
@@ -21,6 +29,44 @@ public class InfoActivity extends ActionBarActivity {
         finish();
         return true;
     }
+    /**
+     * get App versionName
+     * @param context
+     * @return
+     */
+    public String getVersionName(Context context){
+        PackageManager packageManager=context.getPackageManager();
+        PackageInfo packageInfo;
+        String versionName="";
+        try {
+            packageInfo=packageManager.getPackageInfo(context.getPackageName(),0);
+            versionName=packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return versionName;
+    }
 
+    public String getDeviceName() {
+        String manufacturer = Build.MANUFACTURER;
+        String model = Build.MODEL;
+        if (model.startsWith(manufacturer)) {
+            return capitalize(model);
+        } else {
+            return capitalize(manufacturer) + " " + model;
+        }
+    }
+
+    private String capitalize(String s) {
+        if (s == null || s.length() == 0) {
+            return "";
+        }
+        char first = s.charAt(0);
+        if (Character.isUpperCase(first)) {
+            return s;
+        } else {
+            return Character.toUpperCase(first) + s.substring(1);
+        }
+    }
 
 }
