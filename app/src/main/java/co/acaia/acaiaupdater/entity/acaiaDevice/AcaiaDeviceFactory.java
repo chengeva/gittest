a48505830ce701d2ffd950d2c6da7399b9eae110
hanjord <jordan.han@acaia.co>
2021-09-27 14:23:20 +0800

    Fixing list

--- app/src/main/java/co/acaia/acaiaupdater/entity/acaiaDevice/AcaiaDeviceFactory.java
+++ app/src/main/java/co/acaia/acaiaupdater/entity/acaiaDevice/AcaiaDeviceFactory.java
@@ -2,6 +2,7 @@ package co.acaia.acaiaupdater.entity.acaiaDevice;
 
 import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelCinco;
 import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelLunar;
+import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelLunar2021;
 import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelOrion;
 import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelPearl2021;
 import static co.acaia.acaiaupdater.entity.acaiaDevice.AcaiaDevice.modelPearlS;
@@ -11,6 +12,10 @@ public class AcaiaDeviceFactory {
         if(modelName.equals(modelPearl2021)){
             return new Pearl2021(modelName);
         }
+        if(modelName.equals(modelLunar2021)){
+            return new Lunar2021(modelName);
+        }
+
         if(modelName.equals(modelLunar)){
             return new Lunar(modelName);
         }
